5447048fc446c34a7e73e709f47b4321a51abc2f
Robson Brentan <robsonbrentan@gmail.com>
2021-11-01 15:13:33 -0300

    title alteration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
 
         <div className="row px-3">
           <div className="col-sm-6">
-            <h5 className="text-center text-secondary">'Taxa de sucesso (%)</h5>
+            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
             <BarChart />
           </div>
           <div className="col-sm-6">
