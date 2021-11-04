05e31c264794811de2e189bb46d716a2b58c10de
Robson Brentan <robsonbrentan@gmail.com>
2021-11-04 12:09:34 -0300

    Layers

--- backend/src/main/java/com/devsuperior/dsvendas/sevice/SellerService.java
+++ backend/src/main/java/com/devsuperior/dsvendas/sevice/SellerService.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.dsvendas.dto.SellerDTO;
 import com.devsuperior.dsvendas.entities.Seller;
 import com.devsuperior.dsvendas.repositories.SellerRepository;
 
@@ -17,6 +18,6 @@ public class SellerService {
 	
 	public List<SellerDTO> findAll() {
 		List<Seller> result = repository.findAll();
-		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList())
-	}
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+		}
 }
